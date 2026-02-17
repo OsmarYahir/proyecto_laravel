d6d82af6ba88f70454c554d60190d982a943e6ed
Osmar Yahir <yahiresendiz34@gmail.com>
2026-02-17 12:46:47 -0600

    carpetas 5

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -92,7 +92,7 @@
         <a href="/">TIKET MANIA</a>
         
          <a href="/imagenes">Imagenes</a>
-         <a href="/conciertos-crud.index">Gestión Conciertos</a>
+      <a href="{{ route('conciertos-crud.index') }}">Conciertos</a>
         @auth
             <a href="/registro">Mi Cuenta</a>
             <form action="{{ route('logout') }}" method="POST" style="display: inline;">
